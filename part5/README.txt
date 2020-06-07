494d52b50cd2344e38a6812cb8c98a88a3b7377f
Carol Cui <52084940+econcarol@users.noreply.github.com>
2020-06-07 00:25:03 -0700

    Add files via upload

---
+++ part5/README.txt
@@ -0,0 +1,9 @@
+How to make API call to our simple model and analyze a review?
+
+(1) Anaconda Prompt: cd [path to the folder that has app.py]
+(2) Anaconda Prompt: set FLASK_APP = app
+(3) Anaconda Prompy: flask run
+(4) copy the URL where the site is running
+(5) open sentiment.html in a text editor: xhttp.open("POST", "[past the URL here]", true)
+(6) save sentiment.html and open it in a web browser
+(7) enter a review in the textbox and hit "Analyze It!" button to see the predicted sentiment
---
+++ part5/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from predict_sentiment_analysis import get_sentiment
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # make cross-origin AJAX possible
+
+@app.route('/', methods=['GET','POST'])
+def predict():
+	if request.method == 'GET':
+		review = request.args.get('review') 
+	else:
+		review = request.get_json(force=True)['review']
+	if not review:
+		return 'No review found'
+	
+	result = get_sentiment(review)
+	if result == 'pos':
+		sentiment = 'Positive'
+	else:
+		sentiment = 'Negative'
+	return sentiment
+
+if __name__ == '__main__':
+    app.run(debug=False, use_reloader=False, host='0.0.0.0')
